7b32c21a5b7db751375036cbae2cca11e92b5754
Galata <qophaatti@gmail.com>
2026-09-22 04:25:07 -0700

    Update config.js

--- config.js
+++ config.js
@@ -54,7 +54,7 @@ const SITE_CONFIG = {
   showreel: {
     type: "youtube",
     url: "https://youtu.be/PuICQMzOfCk?si=Vhk5m8prkVDIvMVQ",
-    poster: "assets/images/showreel.png",
+    poster: "assets/images/showreel.jpg",
     orientation: "horizontal"
   },
 
